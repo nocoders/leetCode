fb53c98b2562910a6c48bc862f85e940998aec06
fengyunwuji <1812323571@qq.com>
2019-05-27 22:11:51 +0800

    合并两个有序数组

--- src/com/run/simple/array/simple/RemoveDuplicatesArray.java
+++ src/com/run/simple/array/simple/RemoveDuplicatesArray.java
@@ -1,6 +1,8 @@
 package com.run.simple.array.simple;
 
 
+import javax.swing.tree.TreeNode;
+
 /**
 * @Author:         linmeng
 * @CreateDate:     2019/5/25 16:49
@@ -14,8 +16,38 @@ package com.run.simple.array.simple;
 public class RemoveDuplicatesArray {
 
     public static void main(String[] args) {
-        int [] nums={1,3};
-        searchInsert(nums,3);
+        int [] nums1={0};
+        int [] nums2={1};
+
+        merge(nums1,0,nums2,1);
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        return true;
+    }
+    /**
+
+    * 合并两个有序数组
+    * @author      林猛
+    * @date        2019/5/27 20:31
+    */
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int k = m+n-1,i=m-1,j=n-1;k>=0;k--){
+            if (i<0){
+                nums1[k]=nums2[j--];
+                continue;
+            }
+            if (j<0){
+                nums1[k]=nums1[i--];
+                continue;
+            }
+            if (nums1[i]>=nums2[j]){
+                nums1[k]=nums1[i--];
+            }else {
+                nums1[k]=nums2[j--];
+            }
+        }
     }
 
     /**
