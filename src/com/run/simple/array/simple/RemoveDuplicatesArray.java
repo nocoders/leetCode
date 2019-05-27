9f762a02bca0f850fb292354d62ed12eadc0b77a
fengyunwuji <1812323571@qq.com>
2019-05-27 22:51:21 +0800

    二叉树 比较

--- src/com/run/simple/array/simple/RemoveDuplicatesArray.java
+++ src/com/run/simple/array/simple/RemoveDuplicatesArray.java
@@ -22,9 +22,35 @@ public class RemoveDuplicatesArray {
         merge(nums1,0,nums2,1);
     }
 
-    public boolean isSameTree(TreeNode p, TreeNode q) {
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+    /**
+    * @Description: 给定两个二叉树，编写一个函数来检验它们是否相同。
+     *               如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+    * @Author:         linmeng
+    * @CreateDate:     2019/5/27 22:35
+    * @UpdateUser:     linmeng
+    * @UpdateDate:     2019/5/27 22:35
+    * @UpdateRemark:   修改内容
 
-        return true;
+    * @Version:        1.0
+
+    */
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p ==null && q==null){
+            return true;
+        }
+        if (p==null || q==null){
+            return false;
+        }
+        if ( p.val !=q.val){
+            return false;
+        }
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
     }
     /**
 
